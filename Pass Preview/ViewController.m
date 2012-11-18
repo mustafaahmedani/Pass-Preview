a2215a0955d58354c25bbb79e9d2d141555be18c
Mustafa Ahmedani <mustafa_ahmedani@outlook.com>
2012-11-18 12:39:13 +0000

    Added TableView Methods
    Almost all of the TableView methods are in this version of the code.

--- Pass Preview/ViewController.m
+++ Pass Preview/ViewController.m
@@ -9,9 +9,10 @@
 #import "ViewController.h"
 #import <PassKit/PassKit.h>
 
-@interface ViewController () <UITableViewDataSource, UITableViewDelegate, PKAddPassesViewControllerDelegate>
-
-
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, PKAddPassesViewControllerDelegate> {
+    
+    NSMutableArray *_passes;
+}
 
 @end
 
@@ -20,7 +21,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	if (![PKPassLibrary isPassLibraryAvailable]) {
+        
+        [[[UIAlertView alloc] initWithTitle:@"Passbook not available" message:@"The Passbook library is not available" delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil] show];
+        return;
+    }
+    
+    _passes = [[NSMutableArray alloc] init];
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    NSArray *passFiles = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:resourcePath error:nil];
+    for (NSString * passFile in passFiles) {
+        
+        if ( [passFile hasSuffix:@".pkpass"] ) {
+            
+            [_passes addObject:passFile];
+        }
+    }
+}
+
+#pragma mark - Table view
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return _passes.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (!cell) cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    NSString *object = _passes[indexPath.row];
+    cell.textLabel.text = object;
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
