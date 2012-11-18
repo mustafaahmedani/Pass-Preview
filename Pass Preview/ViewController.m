d2e5e79dd5c90a0cb1c95acb9dde1c7baedf1a2f
Mustafa Ahmedani <mustafa_ahmedani@outlook.com>
2012-11-18 11:17:15 +0000

    Added code that presents the passes

--- Pass Preview/ViewController.m
+++ Pass Preview/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <PassKit/PassKit.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, PKAddPassesViewControllerDelegate>{
+    
     NSMutableArray *_passes;
+    
+}
 
 @end
 
@@ -18,7 +22,79 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    if (![PKPassLibrary isPassLibraryAvailable]) {
+        
+        [[[UIAlertView alloc] initWithTitle:@"Error" message:@"PassKit not available" delegate:self cancelButtonTitle:@"Okay" otherButtonTitles: nil] show];
+        return;
+    }
+    
+    _passes = [[NSMutableArray alloc] init];
+    NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+    NSArray *passFiles= [[NSFileManager defaultManager] contentsOfDirectoryAtPath:resourcePath error:nil];
+    for (NSString * passFile in passFiles) {
+        
+        if ([passFile hasSuffix:@".pkpass"]) {
+            
+            [_passes addObject:passFile];
+        }
+    }
+}
+
+#pragma mark - UITableView
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return _passes.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (!cell) cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    
+    NSString *object = _passes[indexPath.row];
+    cell.textLabel.text = object;
+    return cell;
+}
+
+-(void) tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSString *passName = _passes[indexPath.row];
+    [self openPassWithName:passName];
+}
+
+-(void) openPassWithName:(NSString *)name {
+    
+    NSString *passFile = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:name];
+    NSData *passData = [NSData dataWithContentsOfFile:passFile];
+    NSError *error = nil;
+    PKPass *newPass = [[PKPass alloc] initWithData:passData error:&error];
+    if (error!=nil) {
+        [[[UIAlertView alloc] initWithTitle:@"Passes error"
+                                    message:[error
+                                             localizedDescription]
+                                   delegate:nil
+                          cancelButtonTitle:@"Ooops"
+                          otherButtonTitles: nil] show];
+        return;
+    }
+    
+    PKAddPassesViewController *addController = [[PKAddPassesViewController alloc] initWithPass:newPass];
+    addController.delegate = self;
+    [self presentViewController:addController animated:YES completion:nil];
+}
+
+#pragma mark - Pass controller delegate
+
+-(void)addPassesViewControllerDidFinish:(PKAddPassesViewController *)controller {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
